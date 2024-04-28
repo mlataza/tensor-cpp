01e9e4064dd4c37784a6c72dc4d0e3ae333c7cb7
mlataza <1750947+mlataza@users.noreply.github.com>
2024-04-28 18:39:24 +0800

    used ranges for index operations

--- include/tensor-cpp/tensor.hpp
+++ include/tensor-cpp/tensor.hpp
@@ -4,25 +4,41 @@
 #include <array>
 #include <concepts>
 #include <iostream>
+#include <ranges>
 
 #include "sequence.hpp"
 
 namespace tc
 {
-    template <std::size_t Rank>
-    struct row_major_order
+    template <typename ValueType, std::size_t... Shapes>
+        requires(std::is_arithmetic_v<ValueType>)
+    struct tensor
     {
-        using index_type = std::size_t;
-        using array_type = std::array<index_type, Rank>;
+        static constexpr auto container_size = (Shapes * ... * 1);
 
-        static auto flatten(const array_type &shapes, const array_type &indices)
+        using value_type = ValueType;
+        using index_type = std::size_t;
+        using shapes_sequence_type = std::index_sequence<Shapes...>;
+        using indices_type = std::array<index_type, shapes_sequence_type::size()>;
+        using container_type = std::array<value_type, container_size>;
+
+        static constexpr auto shapes_array = sequence::to_array<shapes_sequence_type>::value;
+
+        template <index_type... N>
+        static constexpr auto from_sequence(std::index_sequence<N...>)
+        {
+            return tensor<value_type, N...>{};
+        }
+
+        template <typename ShapesRange, typename IndicesRange>
+        static auto flatten(const ShapesRange &shapes, IndicesRange &&indices)
         {
             index_type index = 0;
 
-            for (index_type k = 0; k < shapes.size(); k++)
+            for (auto it = std::ranges::cbegin(shapes), iit = std::ranges::begin(indices); it != std::ranges::cend(shapes); ++it, ++iit)
             {
-                auto i = indices[k];
-                auto n = shapes[k];
+                auto i = *iit;
+                auto n = *it;
 
                 if (i >= n)
                 {
@@ -35,37 +51,16 @@ namespace tc
             return index;
         }
 
-        static auto expand(const array_type &shapes, index_type index, array_type& indices)
+        template <typename ShapesRange, typename IndicesRange>
+        static auto expand(const ShapesRange &shapes, index_type index, IndicesRange &&indices)
         {
-            for (auto k = indices.size(); k > 0; k--)
+            for (auto rit = std::ranges::crbegin(shapes), riit = std::ranges::rbegin(indices); rit != std::ranges::crend(shapes); ++rit, ++riit)
             {
-                auto i = k - 1;
+                auto shape = *rit;
 
-                indices[i] = index % shapes[i];
-                index /= shapes[i];
+                *riit = index % shape;
+                index /= shape;
             }
-        }
-    };
-
-    template <typename ValueType, std::size_t... Shapes>
-        requires(std::is_arithmetic_v<ValueType>)
-    struct tensor
-    {
-        static constexpr auto container_size = (Shapes * ... * 1);
-
-        using value_type = ValueType;
-        using index_type = std::size_t;
-        using shapes_sequence_type = std::index_sequence<Shapes...>;
-        using indices_type = std::array<index_type, shapes_sequence_type::size()>;
-        using container_type = std::array<value_type, container_size>;
-        using order_type = row_major_order<shapes_sequence_type::size()>;
-
-        static constexpr auto shapes_array = sequence::to_array<shapes_sequence_type>::value;
-
-        template <index_type... N>
-        static constexpr auto from_sequence(std::index_sequence<N...>)
-        {
-            return tensor<value_type, N...>{};
         }
 
         explicit tensor() : _values{} {}
@@ -94,7 +89,7 @@ namespace tc
         constexpr auto &operator()(std::convertible_to<index_type> auto... indices)
             requires(sizeof...(indices) == shapes_sequence_type::size())
         {
-            return _values[order_type::flatten(shapes(), indices_type{static_cast<index_type>(indices)...})];
+            return _values[flatten(shapes(), indices_type{static_cast<index_type>(indices)...})];
         }
 
         constexpr const auto &operator()(std::convertible_to<index_type> auto... indices) const
@@ -216,13 +211,13 @@ namespace tc
             for (index_type i = 0; i < size(); i++)
             {
                 // Get the indices from index
-                order_type::expand(shapes(), i, indices);
+                expand(shapes(), i, indices);
 
                 // Swap the indices of Dim0 and Dim1
                 std::swap(indices[Dim0], indices[Dim1]);
 
                 // Compute the new index for the result
-                auto j = decltype(out)::order_type::flatten(out.shapes(), indices);
+                auto j = flatten(out.shapes(), indices);
 
                 // Copy value to result
                 out[j] = _values[i];
@@ -255,24 +250,24 @@ namespace tc
             // Perform tensor multiplication
             for (index_type i = 0; i < lhs_size; i++)
             {
-                row_major_order<lhs_trim_shapes_sequence_type::size()>::expand(lhs_shapes, i, lhs_trim_indices);
+                expand(lhs_shapes, i, lhs_trim_indices);
 
                 for (index_type j = 0; j < rhs_size; j++)
                 {
-                    row_major_order<rhs_trim_shapes_sequence_type::size()>::expand(rhs_shapes, j, rhs_trim_indices);
+                    expand(rhs_shapes, j, rhs_trim_indices);
 
                     value_type value{};
 
                     for (index_type k = 0; k < common_shape; k++)
                     {
                         std::copy(lhs_trim_indices.begin(), lhs_trim_indices.end(), lhs_indices.begin());
-                        *(lhs_indices.end() - 1) = k;
+                        *std::ranges::rbegin(lhs_indices) = k;
 
                         std::copy(rhs_trim_indices.begin(), rhs_trim_indices.end(), rhs_indices.begin() + 1);
-                        *(rhs_indices.begin()) = k;
+                        *std::ranges::begin(rhs_indices) = k;
 
-                        auto ii = order_type::flatten(shapes(), lhs_indices);
-                        auto jj = row_major_order<sizeof...(RShapes)>::flatten(rhs.shapes(), rhs_indices);
+                        auto ii = flatten(shapes(), lhs_indices);
+                        auto jj = flatten(rhs.shapes(), rhs_indices);
 
                         value += _values[ii] * rhs[jj];
                     }
@@ -280,7 +275,7 @@ namespace tc
                     std::copy(lhs_trim_indices.begin(), lhs_trim_indices.end(), out_indices.begin());
                     std::copy(rhs_trim_indices.begin(), rhs_trim_indices.end(), out_indices.begin() + lhs_trim_indices.size());
 
-                    auto index = row_major_order<out_shapes_sequence_type::size()>::flatten(out.shapes(), out_indices);
+                    auto index = flatten(out.shapes(), out_indices);
                     out[index] = value;
                 }
             }
